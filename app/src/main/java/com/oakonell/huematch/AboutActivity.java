1cfc108249fb4e72119cb79f11143648ce3759cc
Rob O'Connell <oakOnEllGitHub@theoconnells.net>
2017-01-30 15:23:20 -0500

    Touch up about activity

--- app/src/main/java/com/oakonell/huematch/AboutActivity.java
+++ app/src/main/java/com/oakonell/huematch/AboutActivity.java
@@ -10,7 +10,6 @@ import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutActionItem;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
 import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
-import com.danielstone.materialaboutlibrary.model.MaterialAboutTitleItem;
 
 /**
  * Created by Rob on 1/26/2017.
@@ -27,124 +26,59 @@ public class AboutActivity
 
     @Override
     protected MaterialAboutList getMaterialAboutList(Context context) {
-        // TODO get good icons for these
         MaterialAboutCard.Builder appCardBuilder = new MaterialAboutCard.Builder();
 
-        // Add items to card
-
-        appCardBuilder.addItem(new MaterialAboutTitleItem.Builder()
-                .text(R.string.app_name)
-                .icon(R.mipmap.ic_launcher)
-                .build());
-
+        appCardBuilder.addItem(ConvenienceBuilder.createAppTitleItem(context));
         try {
-            appCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(context, context.getDrawable(android.R.drawable.ic_dialog_info),
-//                    new IconicsDrawable(c)
-//                            .icon(GoogleMaterial.Icon.gmd_info_outline)
-//                            .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                            .sizeDp(18),
-                    "Version",
+            appCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(context, context.getDrawable(R.drawable.ic_information_black_24dp),
+                    getString(R.string.about_version),
                     false));
-
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(TAG, "Error adding version card");
         }
 
-//        appCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-//                .text("Changelog")
-//                .icon(new IconicsDrawable(c)
-//                        .icon(CommunityMaterial.Icon.cmd_history)
-//                        .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                        .sizeDp(18))
-//                .setOnClickListener(ConvenienceBuilder.createWebViewDialogOnClickAction(c, "Releases", "https://github.com/daniel-stoneuk/material-about-library/releases", true, false))
-//                .build());
+        appCardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text(R.string.changelog)
+                .icon(R.drawable.ic_book_open_black_24dp)
+                .setOnClickListener(ConvenienceBuilder.createWebViewDialogOnClickAction(context, getString(R.string.changelog), "file:///android_asset/changelog.txt", true, true))
+                .build());
 
-//        appCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-//                .text("Licenses")
-//                .icon(new IconicsDrawable(c)
-//                        .icon(GoogleMaterial.Icon.gmd_book)
-//                        .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                        .sizeDp(18))
-//                .setOnClickListener(new MaterialAboutActionItem.OnClickListener() {
-//                    @Override
-//                    public void onClick() {
-//                        Toast.makeText(
-//                                c, "Material Design About Library with Mike Penz Android Iconics", Toast.LENGTH_SHORT).show();
-//                    }
-//                })
-//                .build());
 
         MaterialAboutCard.Builder authorCardBuilder = new MaterialAboutCard.Builder();
         authorCardBuilder.title(R.string.author);
 
         authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
                 .text("Oak on Ell")
-                .subText("United States")
-                .icon(android.R.drawable.picture_frame)
+                .icon(R.drawable.ic_account_black_24dp)
                 .build());
 
-//        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-//                .text("Fork on GitHub")
-//                .icon(new IconicsDrawable(c)
-//                        .icon(CommunityMaterial.Icon.cmd_github_circle)
-//                        .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                        .sizeDp(18))
-//                .setOnClickListener(ConvenienceBuilder.createWebsiteOnClickAction(c, Uri.parse("https://github.com/daniel-stoneuk")))
-//                .build());
-
-        MaterialAboutCard.Builder convenienceCardBuilder = new MaterialAboutCard.Builder();
-
-        convenienceCardBuilder.title("Convenience Builder");
-//        try {
-//            convenienceCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(c,
-//                    new IconicsDrawable(c)
-//                            .icon(CommunityMaterial.Icon.cmd_information_outline)
-//                            .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                            .sizeDp(18),
-//                    "Version",
-//                    false));
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//        }
-
-        convenienceCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context,
-                context.getDrawable(android.R.drawable.ic_menu_view),
+        authorCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context,
+                context.getDrawable(R.drawable.ic_web_black_24dp),
                 context.getString(R.string.about_visit_web),
                 true,
                 Uri.parse("http://oakonell.com")));
 
-        convenienceCardBuilder.addItem(ConvenienceBuilder.createRateActionItem(context,
-                context.getDrawable(android.R.drawable.star_on),
+        authorCardBuilder.addItem(ConvenienceBuilder.createRateActionItem(context,
+                context.getDrawable(R.drawable.ic_star_black_24dp),
                 context.getString(R.string.about_rate_app),
                 null
         ));
 
-//        convenienceCardBuilder.addItem(ConvenienceBuilder.createEmailItem(c,
-//                new IconicsDrawable(c)
-//                        .icon(CommunityMaterial.Icon.cmd_email)
-//                        .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                        .sizeDp(18),
-//                "Send an email",
-//                true,
-//                "apps@daniel-stone.uk",
-//                "Question concerning MaterialAboutLibrary"));
+
+        MaterialAboutCard.Builder convenienceCardBuilder = new MaterialAboutCard.Builder();
+
+        convenienceCardBuilder.title(R.string.about_libraries);
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context, getDrawable(R.drawable.ic_information_black_24dp),
+                "MaterialAboutLibrary", false, Uri.parse("https://github.com/daniel-stoneuk/material-about-library")));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context, getDrawable(R.drawable.fabric),
+                "Fabric.io - Crashlytics", false, Uri.parse("https://fabric.io/home")));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(context, getDrawable(R.drawable.hue),
+                "Philip Hue SDK", false, Uri.parse("https://developers.meethue.com/")));
 
 
-        MaterialAboutCard.Builder otherCardBuilder = new MaterialAboutCard.Builder();
-        otherCardBuilder.title("Other");
-
-//        otherCardBuilder.addItem(new MaterialAboutActionItem.Builder()
-//                .icon(new IconicsDrawable(c)
-//                        .icon(CommunityMaterial.Icon.cmd_language_html5)
-//                        .color(ContextCompat.getColor(c, R.color.colorIcon))
-//                        .sizeDp(18))
-//                .text("HTML Formatted Sub Text")
-//                .subTextHtml("This is <b>HTML</b> formatted <i>text</i> <br /> This is very cool because it allows lines to get very long which can lead to all kinds of possibilities. <br /> And line breaks.")
-//                .setIconGravity(MaterialAboutActionItem.GRAVITY_TOP)
-//                .build()
-//        );
-
-        return new MaterialAboutList(appCardBuilder.build(), authorCardBuilder.build(), convenienceCardBuilder.build(), otherCardBuilder.build());
+        return new MaterialAboutList(appCardBuilder.build(), authorCardBuilder.build(), convenienceCardBuilder.build());
     }
 
 
